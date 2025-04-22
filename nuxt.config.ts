eec9a4be86a2a9411628a31a445cc17c214089b8
Yurii Myronov <yuramiron16@gmail.com>
2025-04-22 23:12:08 +0200

    create src dir

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,8 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxt/ui', '@nuxt/eslint', '@nuxt/icon'],
 
+  srcDir: 'src/',
+
   runtimeConfig: {
     dbType: 'sqlite',
     dbUrl: 'sqlite::memory:',
