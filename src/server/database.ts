4607003a67422c677c8eb51ff4092f9446f3908f
Yurii Myronov <yuramiron16@gmail.com>
2025-04-24 21:28:37 +0200

    update db connection

--- src/server/database.ts
+++ src/server/database.ts
@@ -1,6 +1,7 @@
-import { Sequelize } from 'sequelize';
+import { Sequelize } from "sequelize";
 
-const config = useRuntimeConfig()
-const sequelize = new Sequelize(config.dbUrl, { logging: config.logging ? console.log : false })
+const config = useRuntimeConfig();
 
-export { sequelize }
+export const sequelize = new Sequelize(config.dbUrl, {
+  logging: config.logging ? console.log : false,
+});
